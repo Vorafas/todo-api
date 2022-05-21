664ef56dfe7b0b9db919637bec931f8be1c3a0e5
Vorafas <safarov.en@yandex.ru>
2022-05-21 13:43:13 +0500

    Initial commit

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItem { get; set; }
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
